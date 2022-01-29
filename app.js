9ebebff2cb771da835011e57edc42bd50e00f47a
eon <noe.navarro1019@gmail.com>
2022-01-28 22:24:33 -0700

    creation of additional sql functionality and updates

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const mainMenu = () => {
         }
     ])
     .then(answer => {
-        addDepartment();
+        addNewRole();
     })
     .then(answer => {
         mainMenu();
@@ -111,7 +111,194 @@ const addDepartment = () => {
     })
 }
 
+// add new role
+const addNewRole = () => {
+    const sql = `
+    INSERT INTO role (title, salary, department_id)
+    VALUES (?, ?, ?)
+    `
 
+    const sql_id = `
+    SELECT id
+    FROM 
+        department
+    WHERE
+        LOWER(name) = ?
+    `
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'role',
+            message: 'What is the role name?'
+        },
+        {
+            type: 'input',
+            name: 'salary',
+            message: "What is this role's salary?"
+        },
+        {
+            type: 'input',
+            name: 'department',
+            message: "What is the depratment's name?"
+        }
+    ])
+    .then(answer => {
+        // separate response values
+        let role = [answer.role];
+        let salary = parseFloat([answer.salary]);
+        let department = [answer.department][0].toLowerCase();
+        
+        // query deparment table for department id
+        let department_id = db.query(sql_id, department, (err, row) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            // replace department id variable with query results
+            department_id = row[0].id;
+
+            // insert new role
+            db.query(sql, [role, salary, department_id], (err, rows) => {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                console.log(`The ${role} has been added to the database!`);
+            })
+        })
+    })
+}
+
+// add new employee
+const addNewEmployee = () => {
+    const sql = `
+    INSERT INTO employee (first_name, last_name, role_id, manager_id)
+    VALUES (?, ?, ?, ?)
+    `
+
+    const sql_manager = `
+    SELECT 
+        role.id as 'role_id',
+        employee.id as 'manager_id'
+    FROM 
+        employee
+    LEFT JOIN 
+        role
+        ON employee.role_id = role.id
+    WHERE 
+        LOWER(employee.first_name) = ?
+        AND LOWER(employee.last_name) = ?
+    `
+
+    inquirer.prompt([
+        {
+            type: 'input',
+            name:'firstName',
+            message: 'What is their first name?'
+        },
+        {
+            type: 'input',
+            name: 'lastName', 
+            message: 'What is their last name?'
+        },
+        {
+            type: 'input',
+            name: 'role',
+            message: 'What is their role?'
+        },
+        {
+            type: 'input',
+            name: 'managerFirst',
+            message: "What is their manager's first name?"
+        },
+        {
+            type: 'input',
+            name: 'managerLast',
+            message: "What is their manager's last name?"
+        }
+    ])
+    .then(answer => {
+        let firstName = [answer.firstName][0];
+        let lastName = [answer.lastName][0]
+        let managerFirst = [answer.managerFirst][0].toLowerCase();
+        let managerLast = [answer.managerLast][0].toLowerCase();
+
+        // query for role and manager
+        db.query(sql_manager, [managerFirst, managerLast], (err, row) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            roleId = row[0].role_id;
+            managerId = row[0].manager_id;
+
+        // insert new employee
+        db.query(sql, [firstName, lastName, roleId, managerId], (err, row) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            console.log(`The new employee ${firstName} ${lastName} has been added!`)
+        })
+        })        
+    })
+}
+
+// update employee value
+const updateEmployee = () => {
+
+    const sql = `
+    UPDATE employee
+    SET role_id = ?
+    WHERE
+        CONCAT(LOWER(first_name), LOWER(last_name)) = ?
+    `
+
+    // employee container 
+    let employeeContainer = []
+
+    let employees = db.query("SELECT CONCAT(first_name, ' ', last_name) as 'employee' FROM employee", (err, row) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        // append each employee to the list to be passed into inquirer
+        row.forEach(item => employeeContainer.push(item.employee))
+
+        // prompt user input for employee
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: 'employee',
+                message: 'Please select the employee to update',
+                choices: employeeContainer
+            },
+            {
+                type: 'input',
+                name: 'newRole',
+                message: 'What is the new role id?'
+            }
+        ])
+        .then(answer => {
+            let employeeName = answer.employee.split(' ').join('').toLowerCase();
+            let newRole = answer.newRole[0];
+
+            console.log(employeeName)
+            console.log(newRole)
+
+            // run update query 
+            db.query(sql, [newRole, employeeName], (err, row) => {
+                if (err) {
+                    console.log(err);
+                    return;
+                }
+                console.log(`The role has been updated!`)
+            })
+        })
+    })
+}
 
 // call the main menu function
-mainMenu();
+// mainMenu();
+
+updateEmployee();
